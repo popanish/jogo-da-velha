ef6baf8b7545915090d0d6d9bdbe6bd2d3d9813f
popanish <matheusq468@gmail.com>
2023-04-07 18:03:50 -0300

    e

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o <code>src/App.js</code> e salve para recarregar.
-      
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendendo React
-        </a>
-      </header>
-    </div>
-  );
+    <a href='#' className='action'>Sobre</a>
+
+    );
 }
 
 export default App;
